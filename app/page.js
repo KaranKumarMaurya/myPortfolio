f2ba1110f92b56b4bc767b167990e1a2f184757b
KaranKumarMaurya <karankumarmaurya2002@gmail.com>
2025-07-10 11:37:52 +0530

    fix 7

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
       {/* <Experience /> */}
       <Skills />
       <Projects />
-      {/* <Education /> */}
+      <Education />
       <Blog />
       <ContactSection />
     </div>
